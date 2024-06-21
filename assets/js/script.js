d647bdb4e7c46c2e0e5e2b6a717b0ae4e3ce3f1d
Carl Jones <saanotherperson@gmail.com>
2024-06-21 10:28:21 +0000

    added random number function to runGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
-function runGame() {
+// Wait for DOM before running game
+// Getting button elements and adding event listeners
 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+/**
+ * The main game loop, called when script is first loaded
+ * and after the user answer is processed
+ */
+function runGame() {
+    // create random no. between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
@@ -31,5 +55,5 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivisionQuestion() {
-    
+
 }

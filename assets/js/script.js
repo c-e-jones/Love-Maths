c6c158cb80db116c71a7f0570a1243de175e52b4
Carl Jones <saanotherperson@gmail.com>
2024-06-24 13:02:04 +0000

    added multiply functionality, subtract almost there

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiple") {
+        disaplyMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting.`
@@ -67,7 +69,9 @@ function calculateCorrectAnswer () {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1*operand2, "multiple"];
+    }    else {
         alert(`Unimplemented operator ${operator}`);
         throw(`Unimplemented operator ${operator}. Aborting.`)
     }
@@ -102,7 +106,9 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
 
 function displayDivisionQuestion() {
